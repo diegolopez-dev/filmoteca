34fea497cefe8648decea2ccd26b2d83f827da8e
diegolopez-dev <diego.teide@gmail.com>
2024-05-10 13:40:23 +0200

    deploy

--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,20 @@
-import { createPool } from 'mysql2/promise'
+// import { createPool } from 'mysql2/promise'
 
-const pool = createPool({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    database: 'Peliculas'
+// const pool = createPool({
+//     host: 'localhost',
+//     port: '3306',
+//     user: 'root',
+//     database: 'Peliculas'
+// })
+
+// export default pool
+
+import { config } from "dotenv";
+import pg  from "pg";
+
+const pool = new pg.Pool({
+    connectionString: process.env.DB_URL,
+    ssl: true
 })
 
 export default pool
