6a8a7ef6f0fdf57aabe7e2028cb24fa6707d53fa
diegolopez-dev <diego.teide@gmail.com>
2024-05-13 22:36:10 +0200

    deploy

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,39 @@
-import { createPool } from 'mysql2/promise'
+// import { createPool } from 'mysql2/promise'
+// import { config } from "dotenv"
+// config()
+
+// const pool = createPool({
+//     host: 'localhost',
+//     port: '3306',
+//     user: 'root',
+//     database: 'Peliculas'
+// })
+
+// 
+
+import pg  from "pg";
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from '../config.js'
 
-const pool = createPool({
-  user: DB_USER,
-  password: DB_PASSWORD,
-  host: DB_HOST,
-  port: DB_PORT,
-  database: DB_NAME
+export const pool = new pg.Pool({
+    connectionString: DB_URL,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME
 })
 
 export default pool
+
+// import { createPool } from 'mysql2/promise'
+// import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from '../config.js'
+
+// const pool = createPool({
+//   user: DB_USER,
+//   password: DB_PASSWORD,
+//   host: DB_HOST,
+//   port: DB_PORT,
+//   database: DB_NAME
+// })
+
+// export default pool
