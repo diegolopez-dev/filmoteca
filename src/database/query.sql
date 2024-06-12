abb1cd7024fac5b8f06b7f57c568af46751e2736
diegolopez-dev <diego.teide@gmail.com>
2024-06-12 13:37:06 +0200

    deploy

--- src/database/query.sql
+++ src/database/query.sql
@@ -2,5 +2,5 @@ CREATE TABLE pelicula(
     id int AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(50) NOT NULL,
     genero VARCHAR(20) NOT NULL,
-    descripcion VARCHAR(10) NOT NULL
+    descripcion VARCHAR(100) NOT NULL
 );
